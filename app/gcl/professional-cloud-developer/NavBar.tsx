1f7f46c2355e6550359a3824f8a873b19ca02f89
myoshizumi <myoshizumi@alumni.berklee.edu>
2026-09-03 17:39:46 +0900

    fix(pcd): prevent scroll jump on target focus in NavBar

--- app/gcl/professional-cloud-developer/NavBar.tsx
+++ app/gcl/professional-cloud-developer/NavBar.tsx
@@ -99,7 +99,7 @@ export function NavBar() {
         if (target) {
             target.scrollIntoView({ behavior: 'smooth' });
             window.history.pushState(null, '', `#${id}`);
-            target.focus();
+            target.focus({ preventScroll: true });
         }
     };
 
